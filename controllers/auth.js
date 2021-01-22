4be7c2a2a20eebe3c633caf60fb04d82f8897e79
EdieAprovita <edieveg316@gmail.com>
2021-01-21 19:44:48 -0600

    Adding more auth controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,32 @@
 const User = require('../models/User')
-const generateToken = require("../utils/generateToken")
-const asyncHandler = require("express-async-handler")
+const generateToken = require('../utils/generateToken')
+const asyncHandler = require('express-async-handler')
 
-const authUser = asyncHandler(async (req,res) => {
+export const authUser = asyncHandler(async (req, res) => {
 	try {
-		
-		const {email, password} = req.body
-	
-		const user = await User.findOne({email})
-	
-		if(user && (await user.matchPassword(password))) {
+		const { email, password } = req.body
+
+		const user = await User.findOne({ email })
+
+		if (user && (await user.matchPassword(password))) {
 			res.json({
-				_id:user._id,
+				_id: user._id,
 				username: user.username,
-				email:user.email,
-				token:generateToken(user._id),
-	
+				email: user.email,
+				token: generateToken(user._id),
 			})
 		}
 	} catch (error) {
-		res.status(401).json({message: `${error}`})
+		res.status(401).json({ message: `${error}` })
 	}
 })
 
-const registerUser = asyncHandler(async(req,res) => {
+export const registerUser = asyncHandler(async (req, res) => {
 	try {
-		const userExists = await User.findOne({email})
+		const userExists = await User.findOne({ email })
 
-		if(userExists) {
-			res.status(400).json({message: `This user already exist ${userExists}`.red})
+		if (userExists) {
+			res.status(400).json({ message: `This user already exist ${userExists}`.red })
 		}
 
 		const user = await User.create({
@@ -37,7 +35,7 @@ const registerUser = asyncHandler(async(req,res) => {
 			password,
 		})
 
-		if(user) {
+		if (user) {
 			res.status(201).json({
 				_id: user._id,
 				username: user.username,
@@ -50,9 +48,98 @@ const registerUser = asyncHandler(async(req,res) => {
 	}
 })
 
-const getUserProfile = asyncHandler(async(req,res) => {
+export const getUserProfile = asyncHandler(async (req, res) => {
 	try {
-		
+		const user = await User.findById(req.user._id)
+
+		if (user) {
+			res.json({
+				_id: user._id,
+				username: user.username,
+				email: user.email,
+				token: generateToken(user._id),
+			})
+		}
+	} catch (error) {
+		res.status(400).json({ message: `${error}`.red })
+	}
+})
+
+export const updateUserProfile = asyncHandler(async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id)
+
+		if (user) {
+			user.username = req.body.username || user.username
+			user.email = req.body.email || user.email
+
+			if (req.body.password) {
+				user.password = req.body.password
+			}
+
+			const updatedUser = await user.save()
+
+			res.json({
+				_id: updateUser._id,
+				username: updateUser.username,
+				email: updateUser.email,
+				token: generateToken(updatedUser._id),
+			})
+		}
+	} catch (error) {
+		res.status(400).json({ message: `User not found ${error}`.red })
+	}
+})
+
+export const getUser = asyncHandler(async (req, res) => {
+	try {
+		const users = await User.find({})
+		res.status(200).json(users)
+	} catch (error) {
+		res.status(400).json({ message: `${error}`.red })
+	}
+})
+
+export const deleteUser = asyncHandler(async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+
+		if (user) {
+			await user.remove()
+			res.status(200).json({ message: 'User removed' })
+		}
+	} catch (error) {
+		res.status(404).json({ message: `${error}`.red })
+	}
+})
+
+export const getUserById = asyncHandler(async(req,res) => {
+	try {
+		const user = await User.findById(req.params.id).select("-password")
+
+		if(user) {
+			res.status(200).json(user)
+		}
+	} catch (error) {
+				res.status(404).json({ message: `${error}`.red })
+
+	}
+})
+
+export const updateUser = asyncHandler(async(req,res) => {
+	try {
+		const user = await User.findById(req.params.id)
+
+		if(user) {
+			user.username = req.body.username || user.username
+			user.email = req.body.email || user.email
+			
+			const updatedUser = await user.save()
+
+			res.status(200).json({
+				
+			})
+		}
 	} catch (error) {
 		
 	}

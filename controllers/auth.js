5d9c115ea9a72f1d3f5dd1cc657b08d974eaafbc
EdieAprovita <edieveg316@gmail.com>
2021-01-25 11:02:21 -0600

    Change the aunth

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,21 +7,21 @@ const asyncHandler = require('express-async-handler')
 // @access  Public
 
 exports.authUser = asyncHandler(async (req, res) => {
-	try {
-		const { email, password } = req.body
+	const { email, password } = req.body
 
-		const user = await User.findOne({ email })
+	const user = await User.findOne({ email })
 
-		if (user && (await user.matchPassword(password))) {
-			res.json({
-				_id: user._id,
-				username: user.username,
-				email: user.email,
-				token: generateToken(user._id),
-			})
-		}
-	} catch (error) {
-		res.status(401).json({ message: `${error}` })
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		})
+	} else {
+		res.status(401)
+		throw new Error('Invalid email or password')
 	}
 })
 
@@ -31,6 +31,8 @@ exports.authUser = asyncHandler(async (req, res) => {
 
 exports.registerUser = asyncHandler(async (req, res) => {
 	try {
+		const { username, email, password } = req.body
+
 		const userExists = await User.findOne({ email })
 
 		if (userExists) {
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken')
 
-exports.generateToken = id => {
+const generateToken = id => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
 		expiresIn: '1d',
 	})
 }
+
+exports.module = generateToken
